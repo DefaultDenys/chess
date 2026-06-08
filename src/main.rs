a7016910460d9cc63e6572f72eb329388579c090
denys2 <denys.lempl4@gmail.com>
2026-06-08 14:22:34 +0200

    Add draw board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod board;
+
+use bevy::prelude::*;
+use board::spawn_board;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Startup, spawn_board)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
